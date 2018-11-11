6bf1fec7f56c84a5b6223fbecea43dad2157e1c4
dmatrixx <44583139+dmatrixx@users.noreply.github.com>
2018-11-12 04:59:39 +0530

    Update virustotal_subdomains.py

--- knockpy/modules/virustotal_subdomains.py
+++ knockpy/modules/virustotal_subdomains.py
@@ -1,11 +1,11 @@
 import os.path
 
 _ROOT = os.path.abspath(os.path.dirname(__file__))
-file = os.path.join(_ROOT, 'FQDN.txt')
+file = os.path.join(_ROOT, '', 'FQDN.txt')
 
 def get_subdomains():
     d = []
     with open(file) as f:
         for line in f:
-            d.append(line)
+            d.append(line.replace('\n',''))
     return d
