db18a80ad856a79718d2ecd1668e148a54163e6a
dmatrixx <44583139+dmatrixx@users.noreply.github.com>
2018-11-12 04:34:23 +0530

    Update virustotal_subdomains.py

--- knockpy/modules/virustotal_subdomains.py
+++ knockpy/modules/virustotal_subdomains.py
@@ -1,12 +1,11 @@
-import json
-import urllib
+import os.path
 
-def get_subdomains(domain, apikey):
-	url = 'https://www.virustotal.com/vtapi/v2/domain/report'
-	parameters = {'domain': domain, 'apikey': apikey}
-	try:
-		response = urllib.urlopen('%s?%s' % (url, urllib.urlencode(parameters))).read()
-		response_dict = json.loads(response)
-		return response_dict['subdomains']
-	except:
-		return False
+_ROOT = os.path.abspath(os.path.dirname(__file__))
+file = os.path.join(_ROOT, '', 'FQDN.txt')
+
+def get_subdomains():
+    d = []
+    with open(file) as f:
+        for line in f:
+            d.append(line)
+    return d
