9c78d95d2c05c20974bdf9e48912b1779de49253
dmatrix <44583139+dmatrixx@users.noreply.github.com>
2018-12-05 03:58:01 +0530

    Update knockpy.py

--- knockpy/knockpy.py
+++ knockpy/knockpy.py
@@ -145,7 +145,10 @@ def main():
 	save_scan_json = args.json
 	
         
-	day = str({:02d}.format(datetime.now().day))+"-"+str({:02d}.format(datetime.now().month))+"-"+str(datetime.now().year)
+	d = %02d.format(datetime.now().day)
+	m = %02d.format(datetime.now().month)
+	y = %04d.format(datetime.now().year)
+	day = str(d)+"-"+str(m)+"-"+str(y)
 	root = '/root/asset'
 	f2 = os.path.join(root,target,day,'FQDN.txt')
 	
